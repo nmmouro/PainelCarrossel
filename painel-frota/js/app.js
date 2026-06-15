78d9e3050f761cb3314a83f8e036ec6486582be0
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-15 20:31:01 -0300

    Update app.js

--- painel-frota/js/app.js
+++ painel-frota/js/app.js
@@ -1,1 +1,18 @@
+import { startClock } from "./ui/clock.js";
+import { initAudio } from "./ui/audio.js";
+import { initFullscreen } from "./ui/fullscreen.js";
 
+import { loadPainel } from "./services/painel.js";
+
+startClock();
+
+initAudio();
+
+initFullscreen();
+
+loadPainel();
+
+setInterval(
+loadPainel,
+30000
+);
