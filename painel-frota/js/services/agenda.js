0dab80cc7cc703fc29fbcc83662cbe302b6c3f68
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-15 20:40:21 -0300

    Update agenda.js

--- painel-frota/js/services/agenda.js
+++ painel-frota/js/services/agenda.js
@@ -1,1 +1,12 @@
+import { fetchSheet } from "../api.js";
+import { getCell } from "../helpers.js";
+import { renderTable } from "../render/tables.js";
+import { TABS } from "../config.js";
 
+export async function loadAgenda(){
+
+    await Promise.all([
+        carregarAgendaDia(),
+        carregarAgendaSocial()
+    ]);
+}
