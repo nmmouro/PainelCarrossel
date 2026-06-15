e7d6c214923df9407c68473c569196cf71d84e12
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-15 20:29:52 -0300

    Update clock.js

--- painel-frota/js/ui/clock.js
+++ painel-frota/js/ui/clock.js
@@ -1,1 +1,28 @@
+export function startClock(){
 
+function update(){
+
+const now = new Date();
+
+document.getElementById(
+"clock"
+).innerText =
+
+now.toLocaleDateString("pt-BR")
++
+" — "
++
+now.toLocaleTimeString(
+"pt-BR",
+{
+hour:"2-digit",
+minute:"2-digit"
+}
+);
+
+}
+
+update();
+
+setInterval(update,1000);
+}
