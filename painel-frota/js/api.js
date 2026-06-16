d9ee7221be0147500d217a644ec3b6db959e2e9b
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-15 21:21:58 -0300

    Update api.js

--- painel-frota/js/api.js
+++ painel-frota/js/api.js
@@ -1,17 +1,19 @@
-import { SHEET_ID } from "./config.js";
-import { parseGviz } from "./helpers.js";
+async function fetchSheet(sheet){
 
-export async function fetchSheet(sheet, query){
+  let query = "";
 
-    const url =
-    `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq`+
-    `?sheet=${encodeURIComponent(sheet)}`+
-    `&tq=${encodeURIComponent(query)}`+
+  if(sheet === "LANÇAMENTOS") query = "select B,C,D,E,F,G,X";
+  if(sheet === "AGENDA DO DIA") query = "select B,C,D,E,F,G,H";
+  if(sheet === "AGENDA SERVIÇO SOCIAL") query = "select B,C,D,F,G,H";
+  if(sheet === "VEÍCULOS") query = "select B,V,M,N";
+  if(sheet === "MOTORISTAS") query = "select A,B,C";
+
+  const url =
+    `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq` +
+    `?sheet=${encodeURIComponent(sheet)}` +
+    `&tq=${encodeURIComponent(query)}` +
     `&tqx=out:json`;
 
-    const res = await fetch(url);
-
-    return parseGviz(
-        await res.text()
-    );
+  const res = await fetch(url);
+  return parseGviz(await res.text());
 }
