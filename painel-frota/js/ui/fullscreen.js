f3df9292661e4ee03671f96dd1b34a7da77d5db2
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-16 11:23:00 -0300

    Delete painel-frota/js/ui/fullscreen.js

--- painel-frota/js/ui/fullscreen.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("toggle-fullscreen").onclick = async ()=>{
-  if(!document.fullscreenElement){
-    await document.documentElement.requestFullscreen();
-  }else{
-    await document.exitFullscreen();
-  }
-};
